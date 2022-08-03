91c15bf14bda99c4ee00e2723bbefb6ad5a1d8d1
Marame Khelil <102168288+MarameKhelil@users.noreply.github.com>
2022-08-03 23:27:04 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+import '../../App.css'
+import Acceuilsection from '../Acceuilsection'
+function Home (){
+    return(
+        <>
+           <Acceuilsection/>;
+        </>
+    )
+}
+export default Home ;
